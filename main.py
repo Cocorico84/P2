e82973b7f51f89408daa899082033689f3690ffc
Corentin <corentin@fanprime.io>
2021-02-22 15:40:20 +0100

    create functions

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import re
 
-url = "http://books.toscrape.com/"
 
-# response = requests.get(url)
-# soup = BeautifulSoup(response.text, 'html.parser')
-
-# raw_links = soup.findAll('h3')
-# links = [i.contents[0]['href'] for i in raw_links]
-
-# next_page = url + soup.findAll(attrs={"class": 'next'})[0].contents[0]['href']
-
-links = []
-for n in range(1, 51):
-    next_page = f'http://books.toscrape.com/catalogue/page-{n}.html'
-    response = requests.get(next_page)
+def get_soup(url):
+    response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    raw_links = soup.findAll('h3')
-    for i in raw_links:
-        links.append(i.contents[0]['href'])
+    return soup
 
 
-print(len(links))
+def get_all_links_by_category(soup):
+    categories = soup.findAll("a")
+
+    category_links = []
+    for i in categories:
+        if 'category' in i.get('href'):
+            category_links.append(i.get('href'))
+    return category_links
 
 
+def get_all_books_from_category(category_url):
+    links = []
+    page = 1
+    url = "http://books.toscrape.com/" + category_url.replace('/index.html', f'/page-{page}.html')
+    while True:
+        if requests.get(url).status_code == 200:
+            book_links = get_soup(url).findAll('h3')
+            for i in book_links:
+                link = "http://books.toscrape.com/catalogue/" + i.contents[0]['href'][9:]
+                links.append(link)
+            page += 1
+        else:
+            break
+        url = "http://books.toscrape.com/" + category_url.replace('/index.html', f'/page-{page}.html')
+    return links
 
+
+def get_data_from_book(book_urls):
+    all_data_from_from_from_category = []
+    for url in book_urls:
+        keys = [i.contents[0] for i in get_soup(url).findAll('th')]
+        values = [i.contents[0] for i in get_soup(url).findAll('td')]
+        data = dict(zip(keys, values))
+        data['Title'] = get_soup(url).find('h1').contents[0]
+        data['Category'] = get_soup(url).find_all(href=re.compile("category"))[1].contents[0]
+        data['Image src'] = "http://books.toscrape.com/" + get_soup(url).find('img')['src'][6:]
+        data['Description'] = get_soup(url).select('article > p')[0].contents[0]
+        data['Product page url'] = url
+        all_data_from_from_from_category.append(data)
+    return pd.DataFrame(all_data_from_from_from_category)
+
+
+def create_csv(csv_name, book_urls):
+    pd.DataFrame.to_csv(get_data_from_book(book_urls), f'{csv_name}.csv')
+
+
+if __name__ == '__main__':
+    url = "http://books.toscrape.com/"
+    categories = get_all_links_by_category(get_soup(url))[1:]
+    category_book_links = get_all_books_from_category(categories[1])
+    create_csv('my_csv', category_book_links)
