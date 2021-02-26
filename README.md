2af8df4061c2f8461443cad813e4f9b0bb47ef44
Corentin <corentin@fanprime.io>
2021-02-26 19:00:38 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+## Set up your virtual environment
+
+```bash
+pip3 install virtualenv
+virtualenv venv --python=python3
+source venv/bin/activate
+```
+
+## Usage
+
+```bash
+python3 main.py
+
+```
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 import re
 import os
 
+URL = "http://books.toscrape.com/"
+
 
 def get_soup(url):
     response = requests.get(url)
@@ -23,15 +25,15 @@ def get_all_links_by_category(soup):
 
 def get_all_books_from_category(category_url):
     links = []
-    for i in get_soup("http://books.toscrape.com/" + category_url).findAll('h3'):
-        links.append("http://books.toscrape.com/catalogue/" + i.contents[0]['href'][9:])
+    for i in get_soup(URL + category_url).findAll('h3'):
+        links.append(URL + "catalogue/" + i.contents[0]['href'][9:])
     page = 2
     while True:
-        url = "http://books.toscrape.com/" + category_url.replace('/index.html', f'/page-{page}.html')
+        url = URL + category_url.replace('/index.html', f'/page-{page}.html')
         if requests.get(url).status_code == 200:
             book_links = get_soup(url).findAll('h3')
             for i in book_links:
-                link = "http://books.toscrape.com/catalogue/" + i.contents[0]['href'][9:]
+                link = URL + "catalogue/" + i.contents[0]['href'][9:]
                 links.append(link)
             page += 1
         else:
@@ -41,22 +43,21 @@ def get_all_books_from_category(category_url):
 
 def get_data_from_book(book_urls):
     all_data_from_from_from_category = []
+    pattern = re.compile("category")
     for url in book_urls:
         keys = [i.contents[0] for i in get_soup(url).findAll('th')]
         values = [i.contents[0] for i in get_soup(url).findAll('td')]
         data = dict(zip(keys, values))
-        title = get_soup(url).find('h1').contents[0]
+        title = str(get_soup(url).find('h1').contents[0]).replace('/', ' ')
         data['Title'] = title
-        category = get_soup(url).find_all(href=re.compile("category"))[1].contents[0]
+        category = get_soup(url).find_all(href=pattern)[1].contents[0]
         data['Category'] = category
         image_link = "http://books.toscrape.com/" + get_soup(url).find('img')['src'][6:]
         data['Image src'] = image_link
         response = requests.get(image_link)
-        if not os.path.exists(f"{category}"):
-            os.makedirs(f"{category}")
-        file = open(f"{category}/{title}.jpg", "wb")
-        file.write(response.content)
-        file.close()
+        os.makedirs(f"{category}_images", exist_ok=True)
+        with open(f"{category}_images/{title}.jpg", "wb") as file:
+            file.write(response.content)
         data['Description'] = get_soup(url).select('article > p')[0].contents[0]
         data['Product page url'] = url
         all_data_from_from_from_category.append(data)
@@ -68,13 +69,21 @@ def create_csv(csv_name, book_urls):
 
 
 if __name__ == '__main__':
-    url = "http://books.toscrape.com/"
-    categories = get_all_links_by_category(get_soup(url))[1:]
-    cat = [re.findall(r"books/(?P<cat>.*)_\d+/", i)[0] for i in categories]
+    categories = get_all_links_by_category(get_soup(URL))[1:]
+    pattern = re.compile(r"books/(.*)_\d+/")
+    cat = [re.findall(pattern, i)[0] for i in categories]
     categories_dict = {}
     for i, j in enumerate(cat):
         categories_dict[i] = j
         print(i, j)
+    print(50, "all", sep=" ")
     choice = int(input("What category do you want ? : "))
-    category_book_links = get_all_books_from_category(categories[choice])
-    create_csv(f'{categories_dict[choice]}_csv', category_book_links)
+    if 0 <= choice <= 49:
+        category_book_links = get_all_books_from_category(categories[choice])
+        create_csv(f'{categories_dict[choice]}_csv', category_book_links)
+    elif choice == 50:
+        for i in range(max(categories_dict, key=categories_dict.get)):
+            category_book_links = get_all_books_from_category(categories[i])
+            create_csv(f'{categories_dict[i]}_csv', category_book_links)
+    else:
+        print("Wrong number")
